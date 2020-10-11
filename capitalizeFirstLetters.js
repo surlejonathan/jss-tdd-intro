c8fba1636f5d87797191f783da62efd469e720ef
surlejonathan <surle.jonathan@gmail.com>
2020-10-11 17:29:33 +0200

    tests passed

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,18 @@
 const assert = require('assert');
 
+const capitalizeFirstLetters = (string) => {
+    const words = string.split(' ');
+    let newString = '';
+
+    for (let word of words) {
+        if (string.length > 0) {
+            newString += word[0].toUpperCase() + word.slice(1) + " ";
+        } else {
+            newString = '';
+        }   
+    }
+    return newString.trim();
+}
 
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
